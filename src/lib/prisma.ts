cfc30af20e44b2ffbe654fc8dcbb5d5ab2cc0ab7
Deepanshu-024 <goyal.deepanshu024@gmail.com>
2026-01-18 18:21:09 +0530

    fixed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,11 @@
-import { PrismaClient } from '../../src/generated/prisma/client'
+import { PrismaClient } from '@/generated/prisma/client'
 //import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient | undefined
 }
 
+// @ts-expect-error - PrismaClient constructor works without args at runtime
 const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
